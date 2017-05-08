8da4bb91e43578684171b1fb6698708d509abc76
BumagniyPacket <bumagniypacket@yandex.ru>
2017-05-08 15:40:33 +0300

    Updated: - normalize_image function - refactoring

--- mosaic/controller/Image2Mosaic.py
+++ mosaic/controller/Image2Mosaic.py
@@ -2,6 +2,7 @@ import os
 from PIL import Image
 
 FRAGMENT_SHAPE = 30
+MINIMAL_SHAPE = FRAGMENT_SHAPE * 3
 
 
 def fill_color_map(fragment_dir):
@@ -18,52 +19,55 @@ def fill_color_map(fragment_dir):
     return tmp
 
 
+def normalize_image(image):
+    w, h = image.size
+
+    if min(w, h) < MINIMAL_SHAPE:
+        scale = MINIMAL_SHAPE / min(w, h)
+        return image.resize([int(w * scale), int(h * scale)])
+    else:
+        return image
+
+
 def create_mosaic(image_path, color_map, fragments_count=60):
-    """
-
-    :param image_path:
-    :param fragments_count:
-    :param color_map:
-    :return:
-    """
-
     def get_image_by_color(color):
         if color not in color_map.keys():
             return get_image_by_color(color + 1)
         else:
             return color_map[color]
 
-    # open image
     image = Image.open(image_path).convert('L')
-    # grep image size
-    w, h = image.size
-    # check image width. if width < FRAGMENT_SHAPE then resize
-    # to fragment width
-    if w < FRAGMENT_SHAPE:
-        scale = FRAGMENT_SHAPE / w
-        image = image.resize([w * scale, h * scale])
-        w, h = image.size
-    # calculate blank image width an height
-    blank_w = fragments_count
-    blank_h = int(blank_w * (h / w))
-    # create blank image
-    blank = Image.new('L', [blank_w * FRAGMENT_SHAPE,
-                            blank_h * FRAGMENT_SHAPE])
-    # calculate color pick fragment
-    step = int(w / fragments_count)
-    # picking color and put to blank image fragment with needed color
-    for i in range(0, blank_h):
-        for j in range(0, blank_w):
-            fragment_color = image.getpixel((j * step, i * step))
+    normal_image = normalize_image(image)
+    normal_image_w, normal_image_h = normal_image.size
+
+    if normal_image_w < normal_image_h:
+        w_fragments = fragments_count
+        h_fragments = int(w_fragments * (normal_image_h / normal_image_w))
+    else:
+        h_fragments = fragments_count
+        w_fragments = int(h_fragments * (normal_image_w / normal_image_h))
+
+    step = normal_image_w // fragments_count
+
+    blank_w = w_fragments * FRAGMENT_SHAPE
+    blank_h = h_fragments * FRAGMENT_SHAPE
+
+    blank = Image.new('L', [blank_w, blank_h])
+
+    for i in range(0, h_fragments):
+        for j in range(0, w_fragments):
+            fragment_color = normal_image.getpixel((j * step, i * step))
             color_image = get_image_by_color(int(fragment_color))
+
             blank.paste(color_image, (j * FRAGMENT_SHAPE, i * FRAGMENT_SHAPE))
     return blank
 
 
 if __name__ == '__main__':
-    img_path = '../../cat.jpg'
+    img_path = 'test/cat.png'
+    # img_path = 'test/cat_tall.png'
+    img_path = 'test/cat_small.png'
     colormap = fill_color_map('../../img')
-    # make_great_mosaic_again(img_path)
-    mosaic = create_mosaic(img_path, colormap, 60)
-    # mosaic.show()
-    mosaic.save('ret.jpg')
+
+    img = create_mosaic(img_path, colormap)
+    save(img)
