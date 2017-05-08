3f501d5a1258afb294a07ceb1676addb3d9a780d
BumagniyPacket <bumagniypacket@yandex.ru>
2017-05-08 15:38:02 +0300

    test for normalize_function

--- mosaic/controller/test.py
+++ mosaic/controller/test.py
@@ -1,11 +1,30 @@
 import unittest
 
-from .Image2Mosaic import *
+from PIL import Image
+
+from mosaic.controller.Image2Mosaic import (normalize_image,
+                                            MINIMAL_SHAPE)
 
 
-# TODO: nu ya napishu v drugoi raz
 class TestMosaic(unittest.TestCase):
-    pass
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_normalize_image_function(self):
+        normal_image = Image.new('L', [450, 450])
+        small_image = Image.new('L', [20, 20])
+
+        test_image = normalize_image(normal_image)
+        self.assertTrue(min(test_image.size) > MINIMAL_SHAPE)
+        self.assertEqual(normal_image.size, test_image.size)
+
+        test_image = normalize_image(small_image)
+        self.assertEqual(min(test_image.size), MINIMAL_SHAPE)
+        self.assertNotEqual(small_image.size, test_image.size)
+
 
 if __name__ == '__main__':
     unittest.main()
