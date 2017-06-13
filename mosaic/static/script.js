aa1b6fbaf50c333d01c0d7fe956ab25a818f701a
BumagniyPacket <bumagniypacket@yandex.ru>
2017-06-13 22:23:17 +0300

    doublequote

--- mosaic/static/script.js
+++ mosaic/static/script.js
@@ -1,18 +1,18 @@
-$(window).on('load', function () {
-  var $preloader = $('#page-preloader'),
-    $spinner = $preloader.find('.spinner');
+$(window).on("load", function () {
+  var $preloader = $("#page-preloader"),
+    $spinner = $preloader.find(".spinner");
   $spinner.fadeOut();
-  $preloader.delay(350).fadeOut('slow');
+  $preloader.delay(350).fadeOut("slow");
 });
 
-$('.image-wrapper').hide();
-$('.progress').hide();
+$(".image-wrapper").hide();
+$(".progress").hide();
 
 
 $(function () {
-  $('#upload-file-btn').click(function () {
+  $("#upload-file-btn").click(function () {
 
-    var form_data = new FormData($('#upload-file')[0]);
+    var form_data = new FormData($("#upload-file")[0]);
 
     $('.progress').show();
 
@@ -28,11 +28,11 @@ $(function () {
         if (data.message != undefined) {
           alert(data.message);
         }
-        $('#uploadImg').attr("src", data.result);
-        $('.progress').hide();
+        $("#uploadImg").attr("src", data.result);
+        $(".progress").hide();
       },
       error: function (data) {
-        Materialize.toast('Возникла ошибка! Попробуйте еще раз.', 4000);
+        Materialize.toast("Возникла ошибка! Попробуйте еще раз.", 4000);
       }
     });
   });
@@ -42,12 +42,12 @@ function readURL(input) {
   if (input.files && input.files[0]) {
     var reader = new FileReader();
     reader.onload = function (e) {
-      $('#uploadImg').attr("src", e.target.result);
+      $("#uploadImg").attr("src", e.target.result);
     };
     reader.readAsDataURL(input.files[0]);
-    $('.image-wrapper').show();
-    $('.helper').hide();
-    $('.form').css("margin-top", "10px");
+    $(".image-wrapper").show();
+    $(".helper").hide();
+    $(".form").css("margin-top", "10px");
   }
 }
 $("#inputImage").change(function () {
