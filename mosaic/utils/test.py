54a014bc02941df5761941e4ccbdb07c3d28e85e
BumagniyPacket <bumagniypacket@yandex.ru>
2017-07-13 13:10:09 +0300

    changed imports

--- mosaic/utils/test.py
+++ mosaic/utils/test.py
@@ -2,8 +2,7 @@ import unittest
 
 from PIL import Image
 
-from mosaic.controller.Image2Mosaic import (normalize_image,
-                                            MINIMAL_SHAPE)
+from mosaic.utils.image_2_mosaic import normalize_image, MINIMAL_SHAPE
 
 
 class TestMosaic(unittest.TestCase):
@@ -13,15 +12,17 @@ class TestMosaic(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_normalize_image_function(self):
+    def test_normalize_image_function_for_normal_image(self):
         normal_image = Image.new('L', [450, 450])
-        small_image = Image.new('L', [20, 20])
-
         test_image = normalize_image(normal_image)
+
         self.assertTrue(min(test_image.size) > MINIMAL_SHAPE)
         self.assertEqual(normal_image.size, test_image.size)
 
+    def test_normalize_image_function_for_small_image(self):
+        small_image = Image.new('L', [20, 20])
         test_image = normalize_image(small_image)
+
         self.assertEqual(min(test_image.size), MINIMAL_SHAPE)
         self.assertNotEqual(small_image.size, test_image.size)
 
